a77761e7b48f316a5642585da996bd3d1244a2ab
otocampos <otniel41apm@gmail.com>
2024-08-25 23:30:48 -0300

    Update dio_client.dart

--- chapter-11/start/lib/networking/dio_client.dart
+++ chapter-11/start/lib/networking/dio_client.dart
@@ -4,7 +4,7 @@ class DioClient {
   final Dio _dio;
   DioClient() : _dio = Dio() {
     _dio.options = BaseOptions(
-        baseUrl: 'http://192.168.15.8:3000',
+        baseUrl: 'http://localhost:3000',
         contentType: 'application/json',
         receiveTimeout: const Duration(seconds: 15),
         sendTimeout: const Duration(seconds: 15),
